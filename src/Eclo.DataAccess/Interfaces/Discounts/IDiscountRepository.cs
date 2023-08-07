38ecf0472549739a0d3b9b35d0e6295a5d617d03
Sherzod <sherzodyuldashef@gmail.com>
2023-08-07 15:32:40 +0500

    Created IDiscountRepository interface

---
+++ src/Eclo.DataAccess/Interfaces/Discounts/IDiscountRepository.cs
@@ -0,0 +1,10 @@
+﻿using Eclo.DataAccess.Common;
+using Eclo.Domain.Entities.Discounts;
+
+namespace Eclo.DataAccess.Interfaces.Discounts;
+
+public interface IDiscountRepository : IRepository<Discount, Discount>,
+    IGetAll<Discount>
+{
+
+}
