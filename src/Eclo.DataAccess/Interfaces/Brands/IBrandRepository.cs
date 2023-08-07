3e44eef24cf7b8fb23eebe9097b0383661947d31
Sherzod <sherzodyuldashef@gmail.com>
2023-08-07 15:11:44 +0500

    IBrandRepository Create

---
+++ src/Eclo.DataAccess/Interfaces/Brands/IBrandRepository.cs
@@ -0,0 +1,10 @@
+﻿using Eclo.DataAccess.Common;
+using Eclo.Domain.Entities.Brands;
+
+namespace Eclo.DataAccess.Interfaces.Brands;
+
+public interface IBrandRepository : IRepository<Brand, Brand>,
+    IGetAll<Brand>
+{
+
+}
