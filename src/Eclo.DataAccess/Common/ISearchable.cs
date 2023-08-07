e876be9dd85f84e2b7d3ec15e07c82989c59b034
Sherzod <sherzodyuldashef@gmail.com>
2023-08-07 16:38:24 +0500

    Created missed interfaces

---
+++ src/Eclo.DataAccess/Common/ISearchable.cs
@@ -0,0 +1,8 @@
+﻿using Eclo.Application.Utilities;
+
+namespace Eclo.DataAccess.Common;
+
+public interface ISearchable<TModel>
+{
+    public Task<(int ItemsCount, IList<TModel>)> SearchAsync(string search, PaginationParams @params);
+}
---
+++ src/Eclo.DataAccess/Interfaces/Users/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using Eclo.DataAccess.Common;
+using Eclo.DataAccess.ViewModels.Users;
+using Eclo.Domain.Entities.Users;
+
+namespace Eclo.DataAccess.Interfaces.Users;
+
+public interface IUserRepository : IRepository<User, UserViewModel>,
+    IGetAll<UserViewModel>, ISearchable<UserViewModel>
+{
+
+}
