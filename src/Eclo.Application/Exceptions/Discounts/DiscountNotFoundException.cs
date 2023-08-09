d52cfbe2425ee20247b36f79512f8301718d27d5
rajabboyevmirshod10@gmail.com <rajabboyevmirshod10@gmail.com>
2023-08-09 13:06:55 +0500

    Create Discounts Exceptions

---
+++ src/Eclo.Application/Exceptions/Discounts/DiscountNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace Eclo.Application.Exceptions.Discounts;
+
+public class DiscountNotFoundException : NotFoundException
+{
+    public DiscountNotFoundException()
+    {
+        this.TitleMessage = "Discount not found";
+    }
+}
