14cb8a00bd48c83f1093bd52525b7b0ab974d3d0
Sherzod <sherzodyuldashef@gmail.com>
2023-08-11 08:25:02 +0500

    User repository and interface are updated

--- src/Eclo.DataAccess/Interfaces/Users/IUserRepository.cs
+++ src/Eclo.DataAccess/Interfaces/Users/IUserRepository.cs
@@ -7,5 +7,5 @@ namespace Eclo.DataAccess.Interfaces.Users;
 public interface IUserRepository : IRepository<User, UserViewModel>,
     IGetAll<UserViewModel>, ISearchable<UserViewModel>
 {
-
+    public Task<User?> GetByPhoneAsync(string phone);
 }
