d39d2b7a49113dc1d7dd1747233bfa589a7e6936
Sherzod <sherzodyuldashef@gmail.com>
2023-08-15 08:38:00 +0500

    Service security and Configurations layers are written

---
+++ src/Eclo.Services/Security/PasswordHasher.cs
@@ -0,0 +1,15 @@
+﻿namespace Eclo.Services.Security;
+
+public class PasswordHasher
+{
+    public static (string Hash, string Salt) Hash(string password)
+    {
+        string salt = Guid.NewGuid().ToString();
+        string hash = BCrypt.Net.BCrypt.HashPassword(password + salt);
+        return (Hash: hash, Salt: salt);
+    }
+    public static bool Verify(string password, string hash, string salt)
+    {
+        return BCrypt.Net.BCrypt.Verify(password + salt, hash);
+    }
+}
---
+++ src/Eclo.WebApi/Configurations/Layers/ServiceLayerConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Eclo.Services.Interfaces.Auth;
+using Eclo.Services.Interfaces.Brands;
+using Eclo.Services.Interfaces.Categories;
+using Eclo.Services.Interfaces.Common;
+using Eclo.Services.Interfaces.Notifications;
+using Eclo.Services.Services.Auth;
+using Eclo.Services.Services.Brands;
+using Eclo.Services.Services.Categories;
+using Eclo.Services.Services.Common;
+using Eclo.Services.Services.Notifications;
+
+namespace Eclo.WebApi.Configurations.Layers;
+
+public static class ServiceLayerConfiguration
+{
+    public static void ConfigureServiceLayer(this WebApplicationBuilder builder)
+    {
+        //-> DI containers, IoC containers
+        builder.Services.AddScoped<IFileService, FileService>();
+        builder.Services.AddScoped<IPaginator, Paginator>();
+        builder.Services.AddScoped<IAuthService, AuthService>();
+        builder.Services.AddScoped<ITokenService, TokenService>();
+        builder.Services.AddScoped<IBrandService, BrandService>();
+        builder.Services.AddScoped<ICategoryService, CategoryService>();
+        builder.Services.AddSingleton<ISmsSender, SmsSender>();
+    }
+}
