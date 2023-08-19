cd19f7059e394ffdb5fa9c89045891f8e735818e
Sherzod <sherzodyuldashef@gmail.com>
2023-08-19 09:51:26 +0500

    Created product interfaces and services

---
+++ src/Eclo.Services/Interfaces/Products/IProductCommentService.cs
@@ -0,0 +1,20 @@
+﻿using Eclo.Application.Utilities;
+using Eclo.DataAccess.ViewModels.Products;
+using Eclo.Persistence.Dtos.Products;
+
+namespace Eclo.Services.Interfaces.Products;
+
+public interface IProductCommentService
+{
+    public Task<bool> CreateAsync(ProductCommentCreateDto dto);
+
+    public Task<bool> DeleteAsync(long productCommentId);
+
+    public Task<long> CountAsync();
+
+    public Task<IList<ProductCommentViewModel>> GetAllAsync(PaginationParams @params);
+
+    public Task<ProductCommentViewModel> GetByIdAsync(long productCommentId);
+
+    public Task<bool> UpdateAsync(long productCommentId, ProductCommentUpdateDto dto);
+}
---
+++ src/Eclo.Services/Interfaces/Products/IProductDetailFashionService.cs
@@ -0,0 +1,20 @@
+﻿using Eclo.Application.Utilities;
+using Eclo.DataAccess.ViewModels.Products;
+using Eclo.Persistence.Dtos.Products;
+
+namespace Eclo.Services.Interfaces.Products;
+
+public interface IProductDetailFashionService
+{
+    public Task<bool> CreateAsync(ProductDetailFashionCreateDto dto);
+
+    public Task<bool> DeleteAsync(long productDetailFashionId);
+
+    public Task<long> CountAsync();
+
+    public Task<IList<ProductDetailFashionViewModel>> GetAllAsync(PaginationParams @params);
+
+    public Task<ProductDetailFashionViewModel> GetByIdAsync(long productDetailFashionId);
+
+    public Task<bool> UpdateAsync(long productDetailFashionId, ProductDetailFashionUpdateDto dto);
+}
