19ae8308ce2e966211ce3da52a8423b0b0024afd
Sherzod <sherzodyuldashef@gmail.com>
2023-08-19 10:01:29 +0500

    Created user interfaces and services

---
+++ src/Eclo.Services/Interfaces/Users/IAdminUserService.cs
@@ -0,0 +1,17 @@
+﻿using Eclo.Application.Utilities;
+using Eclo.DataAccess.ViewModels.Users;
+
+namespace Eclo.Services.Interfaces.Users;
+
+public interface IAdminUserService
+{
+    public Task<bool> DeleteAsync(long userId);
+
+    public Task<long> CountAsync();
+
+    public Task<IList<AdminUserViewModel>> GetAllAsync(PaginationParams @params);
+
+    public Task<AdminUserViewModel> GetByIdAsync(long userId);
+
+    public Task<IList<AdminUserViewModel>> SearchAsync(string search, PaginationParams @params);
+}
