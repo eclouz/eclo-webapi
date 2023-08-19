2f129298f242ac1eefdb6f7fdbddc617c43dfc41
rajabboyevmirshod10@gmail.com <rajabboyevmirshod10@gmail.com>
2023-08-19 11:49:57 +0500

    Created IDiscountService and IProductService

---
+++ src/Eclo.Services/Interfaces/Discounts/IDiscountService.cs
@@ -0,0 +1,20 @@
+﻿using Eclo.Application.Utilities;
+using Eclo.Domain.Entities.Discounts;
+using Eclo.Persistence.Dtos.Discounts;
+
+namespace Eclo.Services.Interfaces.Discounts;
+
+public interface IDiscountService
+{
+    public Task<bool> CreateAsync(DiscountCreateDto dto);
+
+    public Task<bool> DeleteAsync(long discountId);
+
+    public Task<long> CountAsync();
+
+    public Task<IList<Discount>> GetAllAsync(PaginationParams @params);
+
+    public Task<Discount> GetByIdAsync(long discountId);
+
+    public Task<bool> UpdateAsync(long discountId, DiscountUpdateDto dto);
+}
---
+++ src/Eclo.Services/Interfaces/Discounts/IProductDiscountService.cs
@@ -0,0 +1,20 @@
+﻿using Eclo.Application.Utilities;
+using Eclo.Domain.Entities.Discounts;
+using Eclo.Persistence.Dtos.Discounts;
+
+namespace Eclo.Services.Interfaces.Discounts;
+
+public interface IProductDiscountService
+{
+    public Task<bool> CreateAsync(ProductDiscountCreateDto dto);
+
+    public Task<bool> DeleteAsync(long productDiscountId);
+
+    public Task<long> CountAsync();
+
+    public Task<IList<ProductDiscount>> GetAllAsync(PaginationParams @params);
+
+    public Task<ProductDiscount> GetByIdAsync(long productDiscountId);
+
+    public Task<bool> UpdateAsync(long productDiscountId, ProductDiscountUpdateDto dto);
+}
