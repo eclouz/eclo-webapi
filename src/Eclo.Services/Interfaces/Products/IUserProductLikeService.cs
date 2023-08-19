fbaa6fb0036bd3565eb6d021355203ee40d46e91
rajabboyevmirshod10@gmail.com <rajabboyevmirshod10@gmail.com>
2023-08-19 11:58:04 +0500

    Create IProductDetailSizeService and IUserProductLikeService

---
+++ src/Eclo.Services/Interfaces/Products/IUserProductLikeService.cs
@@ -0,0 +1,20 @@
+﻿using Eclo.Application.Utilities;
+using Eclo.Domain.Entities.Products;
+using Eclo.Persistence.Dtos.Products;
+
+namespace Eclo.Services.Interfaces.Products;
+
+public interface IUserProductLikeService
+{
+    public Task<bool> CreateAsync(UserProductLikeCreateDto dto);
+
+    public Task<bool> DeleteAsync(long userProductLikeId);
+
+    public Task<long> CountAsync();
+
+    public Task<IList<UserProductLike>> GetAllAsync(PaginationParams @params);
+
+    public Task<UserProductLike> GetByIdAsync(long userProductLikeId);
+
+    public Task<bool> UpdateAsync(long userProductLikeId, UserProductLikeUpdateDto dto);
+}
