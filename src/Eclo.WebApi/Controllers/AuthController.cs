e0d807ac049e02425e695fe705165e7b65aa5a1d
Sherzod <sherzodyuldashef@gmail.com>
2023-08-21 20:59:18 +0500

    Auth controllers have been written

---
+++ src/Eclo.WebApi/Controllers/AuthController.cs
@@ -0,0 +1,59 @@
+﻿using Eclo.Persistence.Dtos.Auth;
+using Eclo.Persistence.Validations.Auth;
+using Eclo.Persistence.Validations;
+using Eclo.Services.Interfaces.Auth;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eclo.WebApi.Controllers;
+
+[Route("api/auth")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly IAuthService _authService;
+    public AuthController(IAuthService authService)
+    {
+        this._authService = authService;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> RegisterAsync([FromForm] RegisterDto registerDto)
+    {
+        var validator = new RegisterValidator();
+        var result = validator.Validate(registerDto);
+        if (result.IsValid)
+        {
+            var serviceResult = await _authService.RegisterAsync(registerDto);
+            return Ok(new { serviceResult.Result, serviceResult.CachedMinutes });
+        }
+        else return BadRequest(result.Errors);
+    }
+
+    [HttpPost("register/send-code")]
+    public async Task<IActionResult> SendCodeRegisterAsync(string phone)
+    {
+        var result = PhoneNumberValidator.IsValid(phone);
+        if (result == false) return BadRequest("Phone number is invalid!");
+
+        var serviceResult = await _authService.SendCodeForRegisterAsync(phone);
+        return Ok(new { serviceResult.Result, serviceResult.CachedVerificationMinutes });
+    }
+
+    [HttpPost("register/verify")]
+    public async Task<IActionResult> VerifyRegisterAsync([FromBody] VerifyRegisterDto verifyRegisterDto)
+    {
+        var serviceResult = await _authService.VerifyRegisterAsync(verifyRegisterDto.PhoneNumber, verifyRegisterDto.Code);
+        return Ok(new { serviceResult.Result, serviceResult.Token });
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> LoginAsync([FromBody] LoginDto loginDto)
+    {
+        var validator = new LoginValidator();
+        var valResult = validator.Validate(loginDto);
+        if (valResult.IsValid == false) return BadRequest(valResult.Errors);
+
+        var serviceResult = await _authService.LoginAsync(loginDto);
+        return Ok(new { serviceResult.Result, serviceResult.Token });
+    }
+}
---
+++ src/Eclo.WebApi/Controllers/SmsController.cs
@@ -0,0 +1,22 @@
+﻿using Eclo.Persistence.Dtos.Notifications;
+using Eclo.Services.Interfaces.Notifications;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eclo.WebApi.Controllers;
+
+[Route("api/sms")]
+[ApiController]
+public class SmsController : ControllerBase
+{
+    private readonly ISmsSender _smsSender;
+    public SmsController(ISmsSender smsSender)
+    {
+        this._smsSender = smsSender;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> SendAsync([FromBody] SmsMessage smsMessage)
+    {
+        return Ok(await _smsSender.SendAsync(smsMessage));
+    }
+}
