b1f6584aeb9d14de738765294f563c873919f9f1
Sherzod <sherzodyuldashef@gmail.com>
2023-08-21 12:24:33 +0500

    Product controllers are updated

--- src/Eclo.WebApi/Program.cs
+++ src/Eclo.WebApi/Program.cs
@@ -1,25 +1,32 @@
+using Eclo.WebApi.Configurations.Layers;
+using Eclo.WebApi.Configurations;
+using Microsoft.AspNetCore.Diagnostics;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddMemoryCache();
+builder.ConfigureJwtAuth();
+builder.ConfigureSwaggerAuth();
+//builder.ConfigureCORSPolicy();
+builder.ConfigureDataAccess();
+builder.ConfigureServiceLayer();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
+//app.UseCors("AllowAll");
+app.UseStaticFiles();
+//app.UseMiddleware<ExceptionHandlerMiddleware>();
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
