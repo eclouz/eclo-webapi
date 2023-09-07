9821c25e527cf5b01152f8898d8a9ddb87e81e2f
Abdulaziz <orazbaev.abdulaziz.98@gmail.com>
2023-09-07 20:24:45 +0500

    Enitites for Orders have been written

---
+++ src/Eclo.Domain/Entities/Orders/Order.cs
@@ -0,0 +1,18 @@
+﻿namespace Eclo.Domain.Entities.Orders;
+
+public class Order : Auditable
+{
+    public long UserId { get; set; }
+
+    public float ProductsPrice { get; set; }
+
+    public string Status { get; set; } = String.Empty;
+
+    public string Description { get; set; } = String.Empty;
+
+    public bool IsContracted { get; set; } = false;
+
+    public bool IsPaid { get; set; } = false;
+
+    public string PaymentType { get; set; } = String.Empty;
+}
---
+++ src/Eclo.Domain/Entities/Orders/OrderDetail.cs
@@ -0,0 +1,16 @@
+﻿namespace Eclo.Domain.Entities.Orders;
+
+public class OrderDetail : Auditable
+{
+    public long OrderId { get; set; }
+
+    public long ProductDiscountId { get; set; }
+
+    public int Quantity { get; set; }
+
+    public float Price { get; set; }
+
+    public float DiscountPrice { get; set; }
+
+    public float TotalPrice { get; set; }
+}
