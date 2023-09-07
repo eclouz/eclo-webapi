ae78c59933b546c0aa2eee27c6e659d32ba4b161
Abdulaziz <orazbaev.abdulaziz.98@gmail.com>
2023-09-07 20:44:39 +0500

    IOrderRepository and IOrderDetailRepository have been completed

---
+++ src/Eclo.DataAccess/Interfaces/Orders/IOrderDetailRepository.cs
@@ -0,0 +1,9 @@
+﻿using Eclo.DataAccess.Common;
+using Eclo.Domain.Entities.Orders;
+
+namespace Eclo.DataAccess.Interfaces.Orders;
+
+public interface IOrderDetailRepository : IRepository<OrderDetail, OrderDetail>,
+    IGetAll<OrderDetail>, ISearchable<OrderDetail>
+{
+}
