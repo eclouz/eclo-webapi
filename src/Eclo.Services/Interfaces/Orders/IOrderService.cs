eb37a81f4b8076072a94ff9dd10b0d858b304721
Abdulaziz <orazbaev.abdulaziz.98@gmail.com>
2023-09-08 17:42:13 +0500

    Orders and Payments Interfaces have been created

---
+++ src/Eclo.Services/Interfaces/Orders/IOrderService.cs
@@ -0,0 +1,15 @@
+﻿using Eclo.Application.Utilities;
+using Eclo.Domain.Entities.Orders;
+
+namespace Eclo.Services.Interfaces.Orders;
+
+public interface IOrderService
+{
+    public Task<bool> DeleteAsync(long orderId);
+
+    public Task<long> CountAsync();
+
+    public Task<IList<Order>> GetAllAsync(PaginationParams @params);
+
+    public Task<Order> GetByIdAsync(long orderId);
+}
