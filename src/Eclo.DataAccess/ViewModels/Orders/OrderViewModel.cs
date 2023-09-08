296443577bcee92cafb17ff44ac74cb9428bb6fb
Abdulaziz <orazbaev.abdulaziz.98@gmail.com>
2023-09-08 15:21:24 +0500

    OrderRepository and OrviewModel have been completed

---
+++ src/Eclo.DataAccess/ViewModels/Orders/OrderViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Eclo.DataAccess.ViewModels.Orders;
+
+public class OrderViewModel
+{
+    public long OrderId { get; set; }
+
+    public string ProductName { get; set; } = String.Empty;
+
+    public Int16 DiscountPercentage { get; set; }
+
+    public int Quantity { get; set; }
+
+    public float TotalPrice { get; set; }
+
+    public bool IsContracted { get; set; }
+
+    public bool IsPaid { get; set; }
+
+    public string OrderStatus { get; set; } = String.Empty;
+}
