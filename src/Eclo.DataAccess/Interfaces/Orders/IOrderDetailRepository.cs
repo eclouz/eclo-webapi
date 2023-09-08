ddd9974c4b36cdf23b933d0e3dbea4ff7572957e
Abdulaziz <orazbaev.abdulaziz.98@gmail.com>
2023-09-08 16:09:11 +0500

    Created OrderDetailRepository

--- src/Eclo.DataAccess/Interfaces/Orders/IOrderDetailRepository.cs
+++ src/Eclo.DataAccess/Interfaces/Orders/IOrderDetailRepository.cs
@@ -1,9 +1,10 @@
 ﻿using Eclo.DataAccess.Common;
+using Eclo.DataAccess.ViewModels.Orders;
 using Eclo.Domain.Entities.Orders;
 
 namespace Eclo.DataAccess.Interfaces.Orders;
 
-public interface IOrderDetailRepository : IRepository<OrderDetail, OrderDetail>,
-    IGetAll<OrderDetail>, ISearchable<OrderDetail>
+public interface IOrderDetailRepository : IRepository<OrderDetail, OrderViewModel>,
+    IGetAll<OrderViewModel>, ISearchable<OrderViewModel>
 {
 }
