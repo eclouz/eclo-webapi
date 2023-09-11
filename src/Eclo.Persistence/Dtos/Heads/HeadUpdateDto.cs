2eb4007eee36b376df62d6471128210cb6381cad
Abdulaziz <orazbaev.abdulaziz.98@gmail.com>
2023-09-11 19:23:28 +0500

    HeadUpdateDto has been created

---
+++ src/Eclo.Persistence/Dtos/Heads/HeadUpdateDto.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Eclo.Persistence.DTOs.Heads;
+
+public class HeadUpdateDto
+{
+    public string FirstName { get; set; } = String.Empty;
+
+    public string LastName { get; set; } = String.Empty;
+    
+    public string PhoneNumber { get; set; } = String.Empty;
+
+    public IFormFile? ImagePath { get; set; }
+
+    public string PassportSerialNumber { get; set; } = String.Empty;
+
+    public DateTime BirthDate { get; set; }
+
+    public string Region { get; set; } = String.Empty;
+
+    public string District { get; set; } = String.Empty;
+
+    public string Address { get; set; } = String.Empty;
+}
