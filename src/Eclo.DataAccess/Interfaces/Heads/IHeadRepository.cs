fa69b0a8b2d1598b1c7bb0e56ef1b437d63945fc
Abdulaziz <orazbaev.abdulaziz.98@gmail.com>
2023-09-11 19:21:18 +0500

    Created head repositories and interfaces

---
+++ src/Eclo.DataAccess/Interfaces/Heads/IHeadRepository.cs
@@ -0,0 +1,10 @@
+﻿using Eclo.Domain.Entities.Heads;
+
+namespace Eclo.DataAccess.Interfaces.Heads;
+
+public interface IHeadRepository : IRepository<Head, Head>
+{
+    public Task<Head?> GetByPhoneAsync(string phone);
+    public Task<Head?> GetById(long id);
+    public Task<int> UpdatePhoneNumberAsync(string phoneNumber, Head dto);
+}
