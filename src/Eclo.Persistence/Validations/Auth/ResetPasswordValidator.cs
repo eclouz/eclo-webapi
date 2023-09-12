f05aa0b2fce16300dbee46d75bd13e271c65cafe
Abdulaziz <orazbaev.abdulaziz.98@gmail.com>
2023-09-12 22:31:04 +0500

    Created ResetPasswordValidatorTest

---
+++ src/Eclo.Persistence/Validations/Auth/ResetPasswordValidator.cs
@@ -0,0 +1,16 @@
+﻿using Eclo.Persistence.Dtos.Auth;
+using FluentValidation;
+
+namespace Eclo.Persistence.Validations.Auth;
+
+public class ResetPasswordValidator : AbstractValidator<ResetPasswordDto>
+{
+    public ResetPasswordValidator()
+    {
+        RuleFor(dto => dto.PhoneNumber).Must(phone => PhoneNumberValidator.IsValid(phone))
+            .WithMessage("Phone number is invalid! ex: +998xxYYYAABB");
+
+        RuleFor(dto => dto.Password).Must(password => PasswordValidator.IsStrongPassword(password).IsValid)
+            .WithMessage("Password is not strong password!");
+    }
+}
