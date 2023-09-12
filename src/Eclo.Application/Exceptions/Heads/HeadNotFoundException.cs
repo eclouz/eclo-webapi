0927d6d972a00e0b32b7705e5cb0aa5118647dfd
Abdulaziz <orazbaev.abdulaziz.98@gmail.com>
2023-09-12 19:10:42 +0500

    Created HeadNotFoundException

---
+++ src/Eclo.Application/Exceptions/Heads/HeadNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace Eclo.Application.Exceptions.Heads;
+
+public class HeadNotFoundException : NotFoundException
+{
+    public HeadNotFoundException()
+    {
+        this.TitleMessage = "Head not found!";
+    }
+}
