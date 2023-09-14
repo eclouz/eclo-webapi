280d2c620f8e2cf4a67530a91607c2e0126c6f19
Abdulaziz <orazbaev.abdulaziz.98@gmail.com>
2023-09-14 21:27:01 +0500

    Created / OrderCreate/UpdateDtos and OrderDetailCreate/UpdateDtos

---
+++ src/Eclo.Persistence/Dtos/Orders/OrderCreateDto.cs
@@ -0,0 +1,18 @@
+﻿namespace Eclo.Persistence.Dtos.Orders;
+
+public class OrderCreateDto
+{
+    public long UserId { get; set; }
+
+    public float ProductsPrice { get; set; }
+
+    public string Status { get; set; } = String.Empty;
+
+    public string Description { get; set; } = String.Empty;
+
+    public bool IsContracted { get; set; }
+
+    public bool IsPaid { get; set; }
+
+    public string PaymentType { get; set; } = String.Empty;
+}
