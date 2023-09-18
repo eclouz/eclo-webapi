f2acc3d3bfde86270c96edf9a71bc1f2e680ab16
Abdulaziz <orazbaev.abdulaziz.98@gmail.com>
2023-09-18 22:57:43 +0500

    Changed db connection to digital ocean's postgres server

--- src/Eclo.DataAccess/Repositories/BaseRepository.cs
+++ src/Eclo.DataAccess/Repositories/BaseRepository.cs
@@ -10,9 +10,9 @@ public class BaseRepository
     {
         Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
 
-        /*this._connection = new NpgsqlConnection("Host=db-postgresql-sgp1-54859-do-user-14651493-0.b.db.ondigitalocean.com; Port=25060; " +
-            "Database=eclo-db; User Id=doadmin; Password=AVNS_KkiVspFxY5HjtY0VR4K;");*/
-        this._connection = new NpgsqlConnection("Host=localhost; Port=5432; " +
-            "Database=eclo-db; User Id=postgres; Password=12345;");
+        this._connection = new NpgsqlConnection("Host=db-postgresql-sgp1-54859-do-user-14651493-0.b.db.ondigitalocean.com; Port=25060; " +
+            "Database=defaultdb; User Id=doadmin; Password=AVNS_KkiVspFxY5HjtY0VR4K;");
+        /*this._connection = new NpgsqlConnection("Host=localhost; Port=5432; " +
+            "Database=eclo-db; User Id=postgres; Password=12345;");*/
     }
 }
