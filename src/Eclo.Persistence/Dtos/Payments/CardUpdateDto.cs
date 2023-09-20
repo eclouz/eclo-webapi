4939c6b27ec74e3d560f0415d0c4f560374df32b
Abdulaziz <orazbaev.abdulaziz.98@gmail.com>
2023-09-20 22:57:46 +0500

    Created DTOs and Validators on cards

---
+++ src/Eclo.Persistence/Dtos/Payments/CardUpdateDto.cs
@@ -0,0 +1,20 @@
+﻿namespace Eclo.Persistence.Dtos.Payments;
+
+public class CardUpdateDto
+{
+    public long UserId { get; set; }
+
+    public string CardHolderName { get; set; } = String.Empty;
+
+    public string CardNumber { get; set; } = String.Empty;
+
+    public double Balance { get; set; }
+
+    public int PinCode { get; set; }
+
+    public int ExpiredMonth { get; set; }
+
+    public int ExpiredYear { get; set; }
+
+    public bool IsActive { get; set; }
+}
